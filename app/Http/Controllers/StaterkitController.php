51187cdd154c120980e1adddd65fd4c5cef614f4
Ashir Ali <muhammadashirali@icloud.com>
2022-08-17 15:54:42 +0500

    home

--- app/Http/Controllers/StaterkitController.php
+++ app/Http/Controllers/StaterkitController.php
@@ -14,7 +14,7 @@ class StaterkitController extends Controller
     public function home()
     {
         $breadcrumbs = [
-            ['link' => "home", 'name' => "Home"], ['name' => "Index"]
+            ['link' => "home", 'name' => "Liquidation"], ['name' => "Home"]
         ];
         return view('/content/home', ['breadcrumbs' => $breadcrumbs]);
     }
