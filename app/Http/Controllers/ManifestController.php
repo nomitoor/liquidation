e99249efe0c962ec11cc001126649f600fe72005
NEXTBRIDGE\ashir.muhammad <muhammadashirali4@gmail.com>
2022-09-13 04:04:19 +0500

    updates

--- app/Http/Controllers/ManifestController.php
+++ app/Http/Controllers/ManifestController.php
@@ -786,6 +786,8 @@ class ManifestController extends Controller
 
             Excel::import(new ManifestCompareImport($filename), $filepath);
 
+            unlink($filepath);
+            
             $all_manifest_to_compare = ManifestCompare::pluck('id')->toArray();
             $all_scanned = ScannedProducts::pluck('id')->toArray();
 
