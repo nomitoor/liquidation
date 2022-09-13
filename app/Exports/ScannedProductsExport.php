cd759398cc59f1fcdbbe9def85646f6efaa6f847
nomitoor <63299036+nomitoor@users.noreply.github.com>
2022-09-13 17:08:50 +0500

    Update ScannedProductsExport.php

--- app/Exports/ScannedProductsExport.php
+++ app/Exports/ScannedProductsExport.php
@@ -34,7 +34,7 @@ class ScannedProductsExport implements FromQuery, WithMapping, WithHeadings, Wit
             $last_id = array_values($this->id)[$count-1];
 
             // dd($first_id, $last_id);
-            return ManifestCompare::whereBetween('id', [$first_id, $last_id]);
+            return ManifestCompare::whereBetween('bol', [$first_id, $last_id]);
         } else {
             return ScannedProducts::where('pallet_id', $this->id);
         }
