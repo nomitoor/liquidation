3c2a6a606b44d7ad16c8a37173add4cfaff26974
NEXTBRIDGE\ashir.muhammad <muhammadashirali4@gmail.com>
2022-10-26 03:36:02 +0500

    asdf

--- app/Http/Controllers/PalletsController.php
+++ app/Http/Controllers/PalletsController.php
@@ -23,7 +23,10 @@ class PalletsController extends Controller
         
         foreach ($pallets as $key => $pallet) {
             $rec = 0;
-            $recovery = ScannedProducts::where('pallet_id', $pallet->id)->get();
+            //$recovery = ScannedProducts::where('pallet_id', $pallet->id)->get();
+            $all_bol_ids = Pallets::where('id', $pallet->id)->get(['bol_ids']);
+            $bol_ids = unserialize($all_bol_ids[0]->bol_ids);
+            $recovery = ScannedProducts::whereIn('bol', $bol_ids)->get();
             foreach ($recovery as $recov) {
                 $rec += $recov->total_recovery;
             }
