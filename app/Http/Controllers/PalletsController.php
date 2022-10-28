2f4b09dd141b003ecd1a89252517989fea3d177d
Ashir Ali <muhammadashirali@icloud.com>
2022-10-28 13:42:48 +0500

    sdfs

--- app/Http/Controllers/PalletsController.php
+++ app/Http/Controllers/PalletsController.php
@@ -32,7 +32,7 @@ class PalletsController extends Controller
             }
             $pallets[$key]['recovery'] = $rec;
             } catch (Throwable $e) {
-                
+                $rec = 0;
             }
             //$recovery = ScannedProducts::where('pallet_id', $pallet->id)->get();
             
